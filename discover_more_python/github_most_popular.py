991992da0ab650551df8124d6ceceb68b333c345
Electra Chong <electrachong@gmail.com>
2016-04-03 21:16:34 -0700

    Add first iteration of file that prints Github API response

---
+++ discover_more_python/github_most_popular.py
@@ -0,0 +1,14 @@
+""" This program prints the response from Github's API when requesting 
+    the 30 most popular Python projects
+"""
+from urllib2 import Request, urlopen, URLError
+
+request_headers = {
+    'User-Agent': 'Holberton School',
+    'Authorization': 'token  3d59bbfb240064a050cb72476c091bd363e5267c'
+}
+
+request = Request('https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc')
+response = urlopen(request)
+string = response.read()
+print string
