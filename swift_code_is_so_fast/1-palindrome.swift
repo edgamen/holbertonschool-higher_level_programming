9d7a5012f19004b616cc3a855645f3653ee6f6da
Electra Chong <electrachong@gmail.com>
2016-05-23 16:03:29 -0700

    Simplify code by using String.characters.reverse() instead of manually checking each char

--- swift_code_is_so_fast/1-palindrome.swift
+++ swift_code_is_so_fast/1-palindrome.swift
@@ -1,18 +1,10 @@
 // Return true if string is palindrome, otherwise return false
 func is_palindrome(s: String) -> Bool {
-  var a_index = s.startIndex
-  var b_index = s.endIndex.predecessor()
-  
-  /* to check what is represented by index values:
-  print (a_index), print (b_index) */
-  while a_index < b_index {
-    if s[a_index] == s[b_index] {
-      a_index = a_index.successor()
-      b_index = b_index.predecessor()
-    }
-    else {
-      return false
-    }
+  let reversed_s = String(s.characters.reverse())
+
+  if reversed_s == s {
+     return true
   }
-  return true
+
+  return false
 }
