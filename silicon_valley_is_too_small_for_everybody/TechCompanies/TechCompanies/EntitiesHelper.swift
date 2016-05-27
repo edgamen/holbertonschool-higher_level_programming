bc7867f215d30ed49c3512001a009bcb54a313a5
Electra Chong <electrachong@gmail.com>
2016-05-27 15:54:00 -0700

    Add a EntityHelper class to the Model to retrieve the list of schools and company

---
+++ silicon_valley_is_too_small_for_everybody/TechCompanies/TechCompanies/EntitiesHelper.swift
@@ -0,0 +1,37 @@
+//
+//  EntitiesHelper.swift
+//  TechCompanies
+//
+//  Created by Electra Chong on 5/27/16.
+//  Copyright © 2016 Holberton School. All rights reserved.
+//
+
+import Foundation
+
+class EntityHelper {
+    static var listOfSchool:[Entity]! = []
+    static var listOfTechCompany:[Entity]! = []
+
+    static func getSchools() -> [Entity]!
+    {
+        if listOfSchool.count == 0 {
+            listOfSchool.append(Entity(name:"Holberton", town: "San Francisco", imageName: "holberton", type: .School))
+        }
+        
+        return listOfSchool
+    }
+    
+    static func getTechCompanies() -> [Entity]!
+    {
+        if listOfTechCompany.count == 0 {
+            listOfSchool.append(Entity(name: "Linkedin", town: "San Francisco", imageName: "linkedin", type: .TechCompany))
+            listOfSchool.append(Entity(name: "Docker", town: "San Francisco", imageName: "docker", type: .TechCompany))
+            listOfSchool.append(Entity(name: "Google", town: "Mountain View", imageName: "google", type: .TechCompany))
+            listOfSchool.append(Entity(name: "Yahoo", town: "Sunnyvale", imageName: "yahoo", type: .TechCompany))
+            listOfSchool.append(Entity(name: "Apple", town: "Cupertino", imageName: "apple", type: .TechCompany))
+            listOfSchool.append(Entity(name: "Twitter", town: "San Francisco", imageName: "twitter", type: .TechCompany))
+        }
+        
+        return listOfTechCompany
+    }
+}
