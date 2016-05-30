cefb6ef099617ddde06609755b856fad28aa12fa
Electra Chong <electrachong@gmail.com>
2016-05-29 19:24:10 -0700

    Finish creating Tkinter view - simple menu with an updatable label

--- mvc_let_s_interact_with_print/task_view.py
+++ mvc_let_s_interact_with_print/task_view.py
@@ -6,22 +6,24 @@ import Tkinter as tk
 	the Tkinter Toplevel Widget (class) '''
 class TaskView(tk.Toplevel):
 
-	''' private attributes '''
-	__title_var = tk.StringVar()
-	__title_label 
-
 	def __init__(self, master):
 		''' Check if master is a root Tk() '''
-		if type(master) is not tk.Tk():
+		if master.__class__.__name__ != "Tk":
 			raise Exception("master is not a tk.Tk()")
 
 		''' Initialize default values of parent '''
 		tk.Toplevel.__init__(self, master)
 		''' Set policies for what happens when window is closed '''
 		self.protocol('WM_DELETE_WINDOW', self.master.destroy)
-		
-		self.__title_label = tk.Label(master, text=self.__title_var)
-		self.__title_label.pack(side=right)
+
+                ''' private variables declared with default values '''
+                self.__title_var = tk.StringVar()
+		self.__title_label = tk.Label(self, textvariable=self.__title_var)
+		self.__title_label.pack(side=tk.RIGHT)
+
+                ''' public variables '''
+                self.toggle_button = tk.Button(self, text="Reverse")
+                self.toggle_button.pack(side=tk.LEFT)
 
 	''' public method to update the text in the table '''
 	def update_title(self, title):
