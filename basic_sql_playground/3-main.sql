151fc1945ade63656e20cb7e240275ebd32a7cfe
Electra Chong <electrachong@gmail.com>
2016-06-03 14:40:47 -0700

    Update 3-main.sql so that it selects for the entry to delete in EyesColor based on the id in Person

--- basic_sql_playground/3-main.sql
+++ basic_sql_playground/3-main.sql
@@ -1,5 +1,5 @@
 update Person set age = 27 where first_name = 'Jon' and last_name = 'Snow';
 update Person set age = 18 where first_name = 'Walter Junior' and last_name = 'White';
+delete from EyesColor where person_id = (select id from Person where first_name = 'Walter' and last_name = 'White');
 delete from Person where first_name = 'Walter' and last_name = 'White';
--- need to find out how to delete entry in the other table
 select * from Person order by first_name asc;
