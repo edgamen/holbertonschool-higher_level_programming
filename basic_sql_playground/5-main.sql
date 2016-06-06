950a7c25c6ef604539cfc97a6f13f116e5e331e9
Electra Chong <electrachong@gmail.com>
2016-06-05 20:49:03 -0700

    Add aggregate functions to script

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -6,3 +6,12 @@ INNER JOIN TVShow
       on TVShowPerson.tvshow_id = TVShow.id
 where TVShow.name = 'Game of Thrones'
 order by last_name asc;
+SELECT count(id) from Person where age > 30;
+SELECT Person.id, Person.first_name, Person.last_name, Person.age, EyesColor.color, TVShow.name
+from Person
+INNER JOIN EyesColor
+      on Person.id = EyesColor.person_id
+NATURAL INNER JOIN TVShowPerson
+INNER JOIN TVShow
+      on TVShowPerson.tvshow_id = TVShow.id;
+SELECT sum(age) from Person;
