8e9783086b17de8604133d1876946fe210dd3de5
Electra Chong <electrachong@gmail.com>
2016-06-13 13:05:20 -0700

    Add more functions to main.py

--- databases_are_so_easy_to_manage_in_command_line/main.py
+++ databases_are_so_easy_to_manage_in_command_line/main.py
@@ -16,7 +16,6 @@ def create_tables():
 
 ''' print requested table line by line '''
 def print_table(table, args):
-
     for row in table.select():
         print row
 
@@ -67,6 +66,7 @@ def insert_entry(table, args):
     else:
 	print "Can't insert into User table. Use 'student' if you'd like to insert an entry into Student table."
 
+''' delete a requested entry, by id '''
 def delete_entry(table, args):
     if args == 0:
         print "Please provide an id for the entry to delete."
@@ -80,28 +80,62 @@ def delete_entry(table, args):
         except:
             print "Nothing to delete"
         
-        
-# a hash that lists functions for a given action        
-actions = {'create': insert_entry, 'print': print_table, 'insert': insert_entry, 'delete': delete_entry}
-
-if len(sys.argv) < 2:
-    print "Please enter an action"
-elif sys.argv[1] not in actions.keys():
-    print "Undefined action " + sys.argv[1]
-else:
+''' validate number of arguments and then call function if all good'''
+def call_core_action(action, args):
     my_models_db.connect()
-    selected_action = sys.argv[1]
-    
-    if selected_action == "create":
+        
+    if action == "create":
 	print "Creating tables"
 	create_tables()
-    elif len(sys.argv) < 3:
+    elif len(args) == 0:
         print "Please enter the name of the table you wish to print in lowercase."
     else:
-        requested_table = sys.argv[2]
+        requested_table = args[0]
         model_entries = {'school': School, 'batch': Batch, 'user': User, 'student': Student}
+        core_functions = {'print': print_table, 'insert': insert_entry, 'delete': delete_entry}
         
         if requested_table not in model_entries.keys():
             print "Undefined table: %s. Please make sure to use all lowercase to select table." % requested_table
         else:
-            actions[selected_action](model_entries[requested_table], sys.argv[3:])
+            core_functions[action](model_entries[requested_table], args[1:])
+
+def printby_action(type_object, filter_object, args):
+    my_models_db.connect()
+    
+    if len(args) == 0:
+        print "Please provide a id for the school or batch"
+    elif not is_number(args[0]):
+        print "Please provide a valid integer for the ID of the school or batch."
+    elif (type_object == Student and filter_object == Batch.school):
+        print "dsaj"
+        results = Student.select().join(Batch).where(Batch.school == args[0])
+        for row in results:
+            print row
+    else:
+        print "moop"
+        results = type_object.select().where(filter_object == args[0])
+        for row in results:
+            print row
+            
+# a hash that lists functions for a given action        
+core_actions = ['create', 'print', 'insert', 'delete']
+# defines type of printable object and filter for a specific query
+printby_actions = { \
+    'print_batch_by_school': {'type_object': Batch, 'filter_object': Batch.school}, \
+    'print_student_by_batch': {'type_object': Student, 'filter_object': Student.batch}, \
+    'print_student_by_school': {'type_object': Student, 'filter_object': Batch.school} \
+}
+more_actions = ['print_family', 'age_average', 'change_batch']
+
+if len(sys.argv) < 2:
+    print "Please enter an action"
+elif sys.argv[1] in core_actions:
+    call_core_action(sys.argv[1], sys.argv[2:])
+elif sys.argv[1] in printby_actions.keys():
+    query = printby_actions[sys.argv[1]]
+    print sys.argv[1]
+    print printby_actions[sys.argv[1]]['type_object']
+    print printby_actions[sys.argv[1]]['filter_object']
+    printby_action(query['type_object'], query['filter_object'], sys.argv[2:])
+else:
+    print "Undefined action " + sys.argv[1]
