ae529390c876dfe1ba5ada0716274c9f6ff744e2
Electra Chong <electrachong@gmail.com>
2016-06-15 23:00:26 -0700

    Add a query to display count of episode sby episode number in asc order of episode number

--- mysql_the_world_s_most_popular_open_source_database/1-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/1-main.sql
@@ -1,2 +1,4 @@
-\! echo "Number of seasons by tvshow_id?"
+\! echo "\nNumber of seasons by tvshow_id?"
 select tvshow_id, count(id) from Season group by tvshow_id;
+\!echo "\nNumber of occurrences of the same episode number ordered by episode number?"
+select number, count(id) from Episode group by number order by number;
