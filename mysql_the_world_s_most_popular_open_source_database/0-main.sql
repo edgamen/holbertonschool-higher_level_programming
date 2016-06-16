13bcd0c362ebb4e5901d0da730ed146c9f144d34
Electra Chong <electrachong@gmail.com>
2016-06-15 22:14:09 -0700

    Add a command to show the create statements of some tables

--- mysql_the_world_s_most_popular_open_source_database/0-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/0-main.sql
@@ -1,2 +1,6 @@
 \! echo "\nList of all tables?"
 show tables;
+\! echo "\nDisplay the table structure of TVShow, Genre, and TVShowGenre?"
+show create table TVShow;
+show create table Genre;
+show create table TVShowGenre;
