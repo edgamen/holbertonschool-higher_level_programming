72b7daddcf7828c5df011778c03db4a6a9f10661
Electra Chong <electrachong@gmail.com>
2016-06-16 20:23:40 -0700

    Add a join for Network and TVShow id to display TVshows and the Network names

--- mysql_the_world_s_most_popular_open_source_database/2-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/2-main.sql
@@ -1,2 +1,4 @@
 \! echo "\nNumber of season by TVShow ordered by name (A-Z)?"
 select TVShow.name, count(Season.id) as nb_seasons from Season join TVShow on (Season.tvshow_id = TVShow.id) group by tvshow_id order by name;
+\! echo "\nList of Network by TVShow ordered by name (A-Z)?"
+select TVShow.name as 'TVShow name', Network.name as 'Network name' from TVShow join Network on (TVShow.network_id = Network.id) order by TVShow.name;
