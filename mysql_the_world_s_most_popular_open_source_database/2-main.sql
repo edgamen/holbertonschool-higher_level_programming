e1e81524903adac0aa95ec928404304b44d0fd0a
Electra Chong <electrachong@gmail.com>
2016-06-16 20:29:13 -0700

    Add a query to select tv shows in Fox network

--- mysql_the_world_s_most_popular_open_source_database/2-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/2-main.sql
@@ -2,3 +2,5 @@
 select TVShow.name, count(Season.id) as nb_seasons from Season join TVShow on (Season.tvshow_id = TVShow.id) group by tvshow_id order by name;
 \! echo "\nList of Network by TVShow ordered by name (A-Z)?"
 select TVShow.name as 'TVShow name', Network.name as 'Network name' from TVShow join Network on (TVShow.network_id = Network.id) order by TVShow.name;
+\! echo "\nList of TVShows ordered by name (A-Z) in the Network 'Fox (US)'?"
+select TVShow.name from TVShow join Network on (TVShow.network_id = Network.id) where Network.name = 'FOX (US)';
