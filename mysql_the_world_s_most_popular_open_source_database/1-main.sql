eedad291ba555b40ad42323a60ea7e8935c54ae7
Electra Chong <electrachong@gmail.com>
2016-06-16 19:58:38 -0700

    Finally add a search for a string in TVShow and display in lowercase

--- mysql_the_world_s_most_popular_open_source_database/1-main.sql
+++ mysql_the_world_s_most_popular_open_source_database/1-main.sql
@@ -4,3 +4,5 @@ select tvshow_id, count(id) from Season group by tvshow_id;
 select number, count(id) from Episode group by number order by number;
 \! echo "\nTop 3 of the Genre's occurrences in all TVShows ordered by this number?"
 select genre_id, count(genre_id) as occurrences_genre from TVShowGenre group by genre_id order by count(genre_id) desc limit 3;
+\! echo "\nSearch all TVShow with this letter sequence 'th' case insensitive and display with the name in lowercase?"
+select lower(name) as name from TVShow where name like "%th%"
