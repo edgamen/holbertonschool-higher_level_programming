b020cd79ac274cf51f6c2f28483ee9a82e049bca
Electra Chong <electrachong@gmail.com>
2016-06-21 14:12:36 -0700

    Correct error in init process of parent being overwritten by overloading init

--- divide_and_rule/h_fibonacci.py
+++ divide_and_rule/h_fibonacci.py
@@ -4,11 +4,17 @@ class FibonacciThread(threading.Thread):
 
     ''' override initialization of Thread object '''
     def __init__(self, number):
+        threading.Thread.__init__(self)
         if type(number) is not int:
             raise Exception ("number is not an integer")
         self.number = number
 
     ''' when thread is started, compute fibonacci value of number in constructor '''
     def run(self):
-        fibonacci_number = self.number - 1 + self.number - 2
+        j = 0
+        k = 1
+        for i in range(1, self.number):
+            fibonacci_number = j + k
+            j = k
+            k = fibonacci_number
         print "%d => %d" %(self.number, fibonacci_number)
