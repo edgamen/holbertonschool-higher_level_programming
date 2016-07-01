e4d08941393c529f2d2f5ed5bc41a556c6689768
Electra Chong <electrachong@gmail.com>
2016-06-30 20:16:01 -0700

    Add a class to simulate a store with a semaphore for person capacity

---
+++ splitting_tasks_to_be_faster/h_store.py
@@ -0,0 +1,26 @@
+import threading
+import time
+import random
+
+class Store:
+
+    def __init__(self, item_number, person_capacity):
+        if type(item_number) is not int:
+            raise Exception("Number of items provided is not an int")
+        if type(person_capacity) is not int:
+            raise Exception("Person capacity provided is not an int")
+        self.item_number = item_number
+        self.__sema = threading.Semaphore(person_capacity)
+
+    def enter(self):
+        self.__sema.acquire()
+
+    def buy(self):
+        time.sleep(random.uniform(5,10))
+        if (self.item_number) <= 0:
+            self.__sema.release()
+            return False
+
+        self.item_number -= 1
+        self.__sema.release()
+        return True;
